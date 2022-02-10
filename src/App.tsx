cafea7cbd2dc756177f2d8009a013f31dc664972
Gabriel D. Padua <gabriel.padua@eduzz.com>
2022-02-10 02:32:46 -0300

    ✨feat(project): init config card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Card from './components/Card';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Teste
-      </h1>
+       <h1>Redux essentials</h1>
+       <div className="line">
+        <Card></Card>
+        <Card></Card>
+       </div>
     </div>
   );
 }
