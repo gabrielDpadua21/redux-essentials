577f650371403dfa33d24a3bb2fb81aa14a0519a
Gabriel D. Padua <gabriel.padua@eduzz.com>
2022-02-10 03:32:30 -0300

    💄style(project): cards styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,20 @@ function App() {
     <div className="App">
        <h1>Redux essentials</h1>
        <div className="line">
-        <Card></Card>
-        <Card></Card>
+        <Card title='card #01' color='blue'>
+           content #01
+        </Card>
+        <Card title='card #02' color='green'>
+           content #02
+        </Card>
+       </div>
+       <div className="line">
+        <Card title='card #03'>
+          content #03
+        </Card>
+        <Card title='card #04' color='purple'>
+          content #04
+        </Card>
        </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+interface IProps {
+    title: string,
+    color?: string,
+    children?: any
+}
+
+const COLORS = ['red', 'blue', 'green', 'purple'];
+
+const getColor = (props: IProps): string => {
+    if(props.color && COLORS.includes(props.color)) {
+        return props.color;
+    }
+    return 'red';
+}
+
+const Card = (props: IProps) => {
     return (
-        <div>
-            <h1>
-                Card
-            </h1>
+        <div className={`card ${getColor(props)}`}>
+            <div className="header">
+                <span className="title">
+                    {props.title}
+                </span>
+            </div>
+            <div className="content">
+                {props.children}
+            </div>
         </div>
     );
 }
